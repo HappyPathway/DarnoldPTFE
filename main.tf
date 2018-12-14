0feda7474bf93706705230f18224b58f131dd5a9
David Arnold <dja+djaboxx@sonic.net>
2018-12-14 11:24:51 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+//--------------------------------------------------------------------
+// Variables
+variable "aws_pes_database_pwd" {}
+
+//--------------------------------------------------------------------
+// Workspace Data
+data "terraform_remote_state" "p_tfe_network" {
+  backend = "atlas"
+  config {
+    address = "app.terraform.io"
+    name    = "pTFE/Network"
+  }
+}
+
+//--------------------------------------------------------------------
+// Modules
+module "aws_pes" {
+  source  = "app.terraform.io/pTFE/aws-pes/ptfe"
+  version = "1.2.0"
+
+  aws_instance_type = "m4.large"
+  database_pwd = "${var.aws_pes_database_pwd}"
+  db_subnet_group_name = "darnold-ptfe"
+  namespace = "darnold-ptfe"
+  owner = "darnold@hashicorp.com"
+  private_route53_zone = "false"
+  route53_zone = "hashidemos.io"
+  ssh_key_name = "tfe-demos-darnold"
+  subnet_ids = ["${data.terraform_remote_state.p_tfe_network.public_subnet}"]
+  ttl = "-1"
+  vpc_security_group_ids = ["${data.terraform_remote_state.p_tfe_network.admin_sg}"]
+}
