67b40bc1b6351ee08f9d3b01869bd93a51df4623
David Arnold <dja+djaboxx@sonic.net>
2018-12-14 12:25:42 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,30 @@
 //--------------------------------------------------------------------
 // Variables
+
 variable "aws_pes_database_pwd" {}
 
 //--------------------------------------------------------------------
-// Workspace Data
-data "terraform_remote_state" "p_tfe_network" {
-  backend = "atlas"
-  config {
-    name    = "pTFE/Network"
-  }
+// Modules
+module "aws_network" {
+  source  = "app.terraform.io/pTFE/aws-network/ptfe"
+  version = "1.0.0"
+
+  namespace = "darnold-ptfe"
 }
 
-//--------------------------------------------------------------------
-// Modules
 module "aws_pes" {
   source  = "app.terraform.io/pTFE/aws-pes/ptfe"
   version = "1.4.0"
 
   aws_instance_type = "m4.large"
   database_pwd = "${var.aws_pes_database_pwd}"
-  db_subnet_group_name = "darnold-ptfe"
+  db_subnet_group_name = "${module.aws_network.db_subnet_group_id}"
   namespace = "darnold-ptfe"
-  owner = "darnold@hashicorp.com"
+  owner = "darnold"
   private_route53_zone = "false"
   route53_zone = "hashidemos.io"
   ssh_key_name = "tfe-demos-darnold"
-  subnet_ids = ["${data.terraform_remote_state.p_tfe_network.public_subnet}"]
+  subnet_ids = "${module.aws_network.subnet_ids}"
   ttl = "-1"
-  vpc_security_group_ids = "${data.terraform_remote_state.p_tfe_network.admin_sg}"
+  vpc_security_group_ids = "${module.aws_network.security_group_id}"
 }
